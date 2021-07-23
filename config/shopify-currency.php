86f0f7a188edcd131d136b60025321ddcbaa4427
Lam Duc Trung <vn.trungld@gmail.com>
2021-07-23 22:00:30 +0700

    Update: config

--- config/shopify-currency.php
+++ config/shopify-currency.php
@@ -2,7 +2,7 @@
 
 return [
     'url' => env('SHOPIFY_CURRENCY_URL', 'https://cdn.shopify.com/s/javascripts/currencies.js'),
-    'cache-key' => env('SHOPIFY_CURRENCY_CACHE_KEY', 'shopify_currencies'),
+    'cache-key' => env('SHOPIFY_CURRENCY_CACHE_KEY', 'shopify_currency'),
     'cache-ttl' => env('SHOPIFY_CURRENCY_CACHE_TTL', 60),
     'round-precision' => 2, // Leave it null if you don't want to round the result
 ];
